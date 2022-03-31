8832c1c90fc4b2c393656e5e1a65066476531637
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-03-31 17:56:56 +0600

    completed

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useRef } from 'react';
+import { useState } from 'react';
 
 const AddUser = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const [user, setUser] = useState([]);
+    const handleSubmit = (e)=>{
+        const name =nameRef.current.value;
+        const email =emailRef.current.value;
+        const user = {name,email};
+        // fetch('')
+        fetch('http://localhost:5000/users',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(user)
+        })
+        .then(res=>res.json())
+        .then(data => console.log(data))
+        nameRef.current.value = '';
+        emailRef.current.value = '';
+        e.target.reset();
+        e.preventDefault();
+
+    }
     return (
         <div>
-            <h2>This is Add User</h2>
+            <h2>Please add an User</h2>
+            <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='name' ref={nameRef} />
+                <input type="text" placeholder='Email' ref={emailRef} />
+            <input type="submit" value="Add" />
+            </form>
         </div>
     );
 };
--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const UpdateUser = () => {
+    const {id} = useParams();
+    const [user,setUser] = useState({});
+    // console.log(id);
+    useEffect(()=>{
+        const url = `http://localhost:5000/update/users/${id}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setUser(data))
+    },[])
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2>Update User</h2>
+            <h3>{user.name} : {user.email}</h3>
+
         </div>
     );
 };
